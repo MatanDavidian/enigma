1f637a78cedb029d69f50deffc7d81e3983bbe45
tomdamri1 <tomdamri@gmail.com>
2019-10-31 22:09:03 +0200

    added - plugboard

--- plugboard.py
+++ plugboard.py
@@ -1,5 +1,32 @@
 from translator import Translator
 
-
+"""
+    this class holds the dictionary which simulate the plugboard.
+    with this class you can:
+    -   create this dict.
+    -   connect plugs to each other.
+"""
 class Plugboard(Translator):
+    """
+        Creates a dictionary called "plugs".
+        In this dict. the keys and the values are both CHARS
+        for easy switching, and using.
+    """
+    def __init__(self):
+        self.plugs={}
+        for i in range (65,91):
+            self.plugs[chr(i)] = chr(i)
+        print(self.plugs)
+
+    """
+        Connecting plug1 to plug2 , and plug2 to plug1
+    """
+    def connectPair(self, plug1 , plug2):
+        try:
+            if self.plugs[plug1]==plug1 and self.plugs[plug2]==plug2:
+                self.plugs[plug1],self.plugs[plug2]=plug2,plug1
+        except:
+            print("please use single capital letters only")
+
+
     pass
