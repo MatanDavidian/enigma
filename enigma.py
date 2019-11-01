2bdc2768c8edde017f6ac63c408d20af9730e8b7
matan <matanda1@ac.sce.ac.il>
2019-11-01 15:48:11 +0200

    finished run from driver.py

--- enigma.py
+++ enigma.py
@@ -2,34 +2,39 @@ from substitutor import Substitutor
 
 
 class Enigma(Substitutor):
-    num_of_rotors = 0
+
+    def machine_configuration_process(self, first, first_key, second, second_key, third, third_key):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.set_rotor_order(first, second, third)
+        self.firstRotorIndex = first_key
+        self.secondRotorIndex = second_key
+        self.thirdRotorIndex = third_key
+        self.configured = True
+
     def complete_letter_translation(self, text):
+        if self.configured == True:
+            for c in text:
+                if 'A' <= c <= 'Z':
+                    c_to_num = ord(c) - 65
 
-        for c in text:
-            if 'A' <= c <= 'Z':
-                c_to_num = ord(c) - 65
-
-                Substitutor.circular_shift(self)
-                encrypt_process = Substitutor.letter_index_conversions(self, c_to_num, 5)
-                #print(encrypt_process)
-                encrypt_process = Substitutor.letter_index_conversions(self, encrypt_process, 4)
-                #print(encrypt_process)
-                encrypt_process = Substitutor.letter_index_conversions(self, encrypt_process, 3)
-                #print(encrypt_process)
-                encrypt_process = ord(Substitutor.reflectorConversion[encrypt_process]) - 65
-                #print(encrypt_process)
-                encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, 3)
-                #print(encrypt_process)
-                encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, 4)
-                #print(encrypt_process)
-                encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, 5)
-                #print(encrypt_process)
-                encrypt_process = Substitutor.plugBoard[encrypt_process]
-                print(encrypt_process, end='')
-
-
-e = Enigma()
-encrypt = "ENIGMABYMATANTOMALEXTOMERAAA"
-print("encrypt: " + encrypt)
-print("decrypt: ", end='')
-e.complete_letter_translation(encrypt)
+                    Substitutor.circular_shift(self)
+                    encrypt_process = Substitutor.letter_index_conversions(self, c_to_num, self.first, self.firstRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = Substitutor.letter_index_conversions(self, encrypt_process, self.second, self.secondRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = Substitutor.letter_index_conversions(self, encrypt_process, self.third, self.thirdRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = ord(self.reflectorConversion[encrypt_process]) - 65
+                    #print(encrypt_process)
+                    encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, self.third, self.thirdRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, self.second, self.secondRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = Substitutor.letter_index_reverse_conversions(self, encrypt_process, self.first, self.firstRotorIndex)
+                    #print(encrypt_process)
+                    encrypt_process = self.plugBoard[encrypt_process]
+                    print(encrypt_process, end='')
+        else:
+            print("enigma is not configured")
